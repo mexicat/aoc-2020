087511db558112044a2205009a44252aad46733a
Giacomo Magnanini <gikappa@gmail.com>
2020-12-10 17:48:35 +0100

    day 10

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -1,7 +1,59 @@
 defmodule AdventOfCode.Day10 do
-  def part1(args) do
+  def part1(input) do
+    {j1, j3} =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sort()
+      |> count_joltage()
+
+    j1 * j3
   end
 
-  def part2(args) do
+  def part2(input) do
+    Agent.start_link(&Map.new/0, name: __MODULE__)
+
+    res = input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> find_combinations(0)
+
+    Agent.stop(__MODULE__)
+
+    res
+  end
+
+  def count_joltage(adapters) do
+    {_, j1, j3} =
+      Enum.reduce(adapters, {0, 0, 1}, fn adapter, {last, j1, j3} ->
+        case adapter - last do
+          1 -> {adapter, j1 + 1, j3}
+          3 -> {adapter, j1, j3 + 1}
+          _ -> {adapter, j1, j3}
+        end
+      end)
+
+    {j1, j3}
+  end
+
+  def find_combinations(adapters, adapter) do
+    case Agent.get(__MODULE__, &Map.get(&1, adapter)) do
+      nil ->
+        val = reachable_adapters(adapters, adapter)
+        Agent.update(__MODULE__, &Map.put(&1, adapter, val))
+        val
+
+      x -> x
+    end
+  end
+
+  def reachable_adapters(adapters, adapter) do
+    adapters
+    |> Enum.filter(fn a -> a in (adapter + 1)..(adapter + 3) end)
+    |> case do
+      [] -> 1
+      [a] -> find_combinations(adapters, a)
+      a -> a |> Enum.map(&find_combinations(adapters, &1)) |> Enum.sum()
+    end
   end
 end
--- test/advent_of_code/day_10_test.exs
+++ test/advent_of_code/day_10_test.exs
@@ -3,19 +3,62 @@ defmodule AdventOfCode.Day10Test do
 
   import AdventOfCode.Day10
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input1 """
+  16
+  10
+  15
+  5
+  1
+  11
+  7
+  19
+  6
+  12
+  4
+  """
 
-    assert result
+  @input2 """
+  28
+  33
+  18
+  42
+  31
+  14
+  46
+  20
+  48
+  47
+  24
+  23
+  49
+  45
+  19
+  38
+  39
+  11
+  1
+  32
+  25
+  35
+  8
+  17
+  7
+  9
+  4
+  2
+  34
+  10
+  3
+  """
+
+  # @tag :skip
+  test "part1" do
+    assert part1(@input2) == 220
   end
 
-  @tag :skip
+  # @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
-
-    assert result
+    assert part2(@input1) == 8
+    assert part2(@input2) == 19208
   end
 end
