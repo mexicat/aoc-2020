68ae63b238e15e1503017363d79e5fe3361cff9a
Giacomo Magnanini <gikappa@gmail.com>
2020-12-16 14:46:24 +0100

    day 16

--- lib/advent_of_code/day_16.ex
+++ lib/advent_of_code/day_16.ex
@@ -1,7 +1,87 @@
 defmodule AdventOfCode.Day16 do
-  def part1(args) do
+  def part1(input) do
+    {fields, _my_ticket, tickets} = parse_input(input)
+
+    Enum.reduce(tickets, 0, fn ticket, total ->
+      ticket
+      |> Enum.map(fn ticket_field ->
+        case Enum.find(fields, fn {_, r1, r2} -> ticket_field in r1 or ticket_field in r2 end) do
+          nil -> ticket_field
+          _range -> 0
+        end
+      end)
+      |> Enum.sum()
+      |> Kernel.+(total)
+    end)
   end
 
-  def part2(args) do
+  def part2(input) do
+    {fields, my_ticket, tickets} = parse_input(input)
+
+    # this mega-pipe should be split in different functions for better testability...
+    # find the valid tickets
+    Enum.reduce([my_ticket | tickets], [], fn ticket, valid_tickets ->
+      ticket
+      |> Enum.map(fn ticket_field ->
+        case Enum.find(fields, fn {_, r1, r2} -> ticket_field in r1 or ticket_field in r2 end) do
+          nil -> false
+          _range -> true
+        end
+      end)
+      |> Enum.all?(& &1)
+      |> if(do: [ticket | valid_tickets], else: valid_tickets)
+    end)
+    # zip all the valid tickets into columns
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.with_index()
+    |> Enum.map(fn {col, col_i} ->
+      plaus =
+        Enum.reduce(fields, [], fn {label, r1, r2}, plausible ->
+          if Enum.all?(col, fn f -> f in r1 or f in r2 end),
+            do: [label | plausible],
+            else: plausible
+        end)
+
+      {plaus, col_i}
+    end)
+    # find the correct field for each column
+    |> Enum.sort(fn {p1, _}, {p2, _} -> length(p1) < length(p2) end)
+    |> Enum.reduce(%{}, fn {plaus, i}, assigned ->
+      Enum.reduce_while(plaus, assigned, fn p, ass ->
+        if Map.has_key?(ass, p), do: {:cont, ass}, else: {:halt, Map.put(ass, p, i)}
+      end)
+    end)
+    |> Enum.filter(fn {k, _v} -> String.starts_with?(k, "departure") end)
+    |> Enum.map(fn {_k, v} -> Enum.at(my_ticket, v) end)
+    |> Enum.reduce(&Kernel.*/2)
+  end
+
+  def parse_input(input) do
+    # this parser function is disgusting, maybe refactor with nimbleparsec
+    [fields, my_ticket, tickets] = String.split(input, "\n\n", trim: true)
+
+    fields =
+      fields
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [label, ranges] = String.split(line, ": ")
+        [r1, r2] = String.split(ranges, " or ")
+        [r1a, r1b] = String.split(r1, "-")
+        [r2a, r2b] = String.split(r2, "-")
+
+        {label, String.to_integer(r1a)..String.to_integer(r1b),
+         String.to_integer(r2a)..String.to_integer(r2b)}
+      end)
+
+    [_, my_ticket] = String.split(my_ticket, "\n", trim: true)
+    my_ticket = my_ticket |> String.split(",") |> Enum.map(&String.to_integer/1)
+
+    [_ | tickets] = String.split(tickets, "\n", trim: true)
+
+    tickets =
+      tickets |> Enum.map(fn t -> t |> String.split(",") |> Enum.map(&String.to_integer/1) end)
+
+    {fields, my_ticket, tickets}
   end
 end
--- test/advent_of_code/day_16_test.exs
+++ test/advent_of_code/day_16_test.exs
@@ -3,19 +3,42 @@ defmodule AdventOfCode.Day16Test do
 
   import AdventOfCode.Day16
 
-  @tag :skip
+  # @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = """
+    class: 1-3 or 5-7
+    row: 6-11 or 33-44
+    seat: 13-40 or 45-50
 
-    assert result
+    your ticket:
+    7,1,14
+
+    nearby tickets:
+    7,3,47
+    40,4,50
+    55,2,20
+    38,6,12
+    """
+
+    assert part1(input) == 71
   end
 
-  @tag :skip
+  # @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    # input = """
+    # class: 0-1 or 4-19
+    # row: 0-5 or 8-19
+    # seat: 0-13 or 16-19
 
-    assert result
+    # your ticket:
+    # 11,12,13
+
+    # nearby tickets:
+    # 3,9,18
+    # 15,1,5
+    # 5,14,9
+    # """
+
+    # assert == 1716
   end
 end
